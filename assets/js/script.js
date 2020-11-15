20703272b414758e7f6c91d4b79aa4257a932d23
GazzaJ <johnge71@gmail.com>
2020-11-15 15:22:16 +0000

    Added transparent navbar functionality

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//---Navbar Transparency (https://bootstrapious.com/p/transparent-navbar)----//
+
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+//---  ---//
