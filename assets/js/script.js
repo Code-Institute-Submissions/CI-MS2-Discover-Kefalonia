3ea56179d3194986c3bd710dfd67f81f198c2967
GazzaJ <johnge71@gmail.com>
2020-11-16 17:15:59 +0000

    Embedded a google map

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,34 @@ $(function () {
     });
 });
 
-//---  ---//
+//--------------- Weather Data Script ----------//
+
+//function getData(url, cb) {
+//    var xhr = new XMLHttpRequest();
+//
+//    xhr.open("GET", "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations");
+//    xhr.send();
+//
+//    xhr.onreadystatechange = function() {
+//        if (this.readyState == 4 && this.status == 200) {
+//           cb(JSON.parse(this.responseText));
+//        }
+//    };
+//}
+
+//function printDataToConsole(data) {
+//    console.log(data);
+//}
+
+//getData(printDataToConsole);
+
+//---------------- Google Maps ---------------//
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 38.28109, lng: 20.48903 },
+        zoom: 11,        
+    });   
+}
