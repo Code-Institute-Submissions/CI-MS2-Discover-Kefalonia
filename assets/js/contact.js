ccd9abd149ecbe270be0b25d06d9889fa9c8c0ea
GazzaJ <johnge71@gmail.com>
2020-12-15 14:06:23 +0000

    Testing emailJS functionality

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -16,39 +16,24 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $("#social-tab").click(function() {
-        $("#social-container").toggleClass("social-active");
-        console.log("hover");
+        $("#social-container").toggleClass("social-active");        
     });
 });
 
 
 //------------ EmailJS Email API ---------------//
 
-/*function sendMail(contactForm) {
-    emailjs.send("gmail", "discover_kefalonia", {
-        "from_name": contactForm.first-name.value + contactForm.last-name.value,
-        "reply-to": contactForm.email-address.value,
-        "message": contactForm.message.value,
-        "mobile": contactForm.mobile.value,
-    })
-    .then(function(response) {
-        console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        emailjs.sendForm("gmail", "discover_kefalonia", this)        
+        .then(
+            function(response) {
+                console.log("SUCCESS!", response);                
+        }, 
+            function(error) {
+                console.log('FAILED...', error);
+            });
+        return false;
     });
     return false;
-}
-*/
-
-
-window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function(event) {
-                emailjs.sendForm('gmail', 'discover_kefalonia', this)
-                    .then(function() {
-                        console.log('SUCCESS!');
-                    }, function(error) {
-                        console.log('FAILED...', error);
-                    });
-            });
-        }
-        
+}        
