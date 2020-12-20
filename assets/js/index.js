540dd0bdb736159a418165022c2b09534ae34451
GazzaJ <johnge71@gmail.com>
2020-12-20 14:39:35 +0000

    Minor changes to remove unwanted code

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,3 @@ $("#explore").click(function() {
 //------GSAP Commands------//
 
 gsap.to("#explore", {duration: 2, opacity: 1, transform: "translateY(50vh)", ease: "back"});
-
-//========== Contact Form Button ==========//
-
