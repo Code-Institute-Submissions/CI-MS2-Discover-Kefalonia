0d0294fabc760815e1b0d7819ceeeb060ac48e92
GazzaJ <johnge71@gmail.com>
2020-12-20 14:57:16 +0000

    FIX - error with Explore button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
+//------GSAP Commands------//
+
+gsap.to("#explore", {duration: 2, opacity: 1, transform: "translateY(50vh)", ease: "back"});
+
 //---Page Transition----//
 $("#explore").click(function() {
     location.href = "map.html";
 });
-
-//------GSAP Commands------//
-
-gsap.to("#explore", {duration: 2, opacity: 1, transform: "translateY(50vh)", ease: "back"});
